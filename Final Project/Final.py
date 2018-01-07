ae14b67e4f5294a08f6041a87d9942e270a39ae2
andrew15818 <32392980+andrew15818@users.noreply.github.com>
2018-01-07 10:45:09 +0800

    getting artist id
    Have to get the id first

---
+++ Final Project/Final.py
@@ -0,0 +1,74 @@
+import urllib.request,urllib.error, urllib.parse
+from bs4 import BeautifulSoup
+import spotipy
+import sys
+import spotipy.util as util
+import json
+import requests
+#The spotipy and requests libraries are 
+#external, not part of Python
+
+''' ===Getting the artist name and track title from the Youtube url==='''
+url = input('Enter the url: ')
+try:
+    handle = urllib.request.urlopen(url)
+except:
+    print('Invalid URL, please try again')
+    exit()
+artist = str()
+track = str()
+soup = BeautifulSoup(handle, 'html.parser')
+#the title of the song is in the watch-title tag
+        
+
+try:
+    title = soup.find(class_= 'watch-title')
+except:
+    print('Could not find the information from the given url :(')
+    
+#we want to just get the string with artist and song name
+
+string = str(title.find(text = True))
+string = string.split('-')
+    
+#These strings have 4 blank spaces at the beginning, so we just replace them
+try:
+    artist = string[0].replace('\n', '').replace(' ', '', 4)
+    track = string[1].replace(" ", '', 1)
+except IndexError:
+    print('could not find the information. Try somehting that is formatted a little different maybe :)')
+    exit()
+    
+
+print('We detected the artist is:', artist)
+print('We detected the track is:' , track)
+
+
+
+'''===Now that we have the artist and track title, we can use that information to connect to the Spotify API==='''
+
+#client_id = '29ed730a97614d47855056f3f71ff371'
+
+
+
+#Our secret id number is in the other file, this loop will make secret_id that number
+#this client id is the one givent to my app when registering on Spotify
+
+#this mehtod will automatically renew tokens if it's expired
+token = util.prompt_for_user_token(username = 'andrew15818', scope='user-read-private' , client_id = '29ed730a97614d47855056f3f71ff371', client_secret = 'f1656e76e32e4d3b922f23c1ee5416a4', redirect_uri = 'http://localhost:8888/callback')
+
+if token:
+    print('successfully connected to Spotify.')
+
+def GetArtistId(artist):
+    artist_endpoint = 'https://api.spotify.com/v1/search'
+    criteria = " q = "+artist"type = artist+ Authorization: Bearer" + token
+    print(type(criteria))
+    response = requests.get(artist_endpoint+criteria)
+    try:
+        print(response)
+    except Exception as e:
+        print (e)
+        
+
+GetArtistId(artist)
--- Final Project/connecting to spotify test.py
+++ Final Project/connecting to spotify test.py
@@ -1,12 +1,3 @@
 import spotipy
-import json
-import pprint
-sp = spotipy.Spotify()
-from spotipy.oauth2 import SpotifyClientCredentials
-cid ="29ed730a97614d47855056f3f71ff371"
-secret = "f1656e76e32e4d3b922f23c1ee5416a4"
-client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-sp.trace=False
-artist = 'Paul Van Dyk'
-artist = sp.search(q = artist)
+import requests
+import httplib2
